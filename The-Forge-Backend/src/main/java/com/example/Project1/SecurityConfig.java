a86ecfcfd4d9622de4d492a0f08e21a2b0a31fb8
adarsh tripathi <adarshtripathi262@gmail.com>
2026-07-20 18:45:40 +0530

    this is the commit after i adding the host of my frontend

--- The-Forge-Backend/src/main/java/com/example/Project1/SecurityConfig.java
+++ The-Forge-Backend/src/main/java/com/example/Project1/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-            "http://localhost:3000"
+            "https://personalized-fitness.onrender.com"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
